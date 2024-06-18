fedb18448f3f0a1e5a1bf3bb11d8962d7c394c69
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-06-18 21:20:45 +0530

    Update 28_Find_the_first_repeated_word_in_string.py

--- 28_Find_the_first_repeated_word_in_string.py
+++ 28_Find_the_first_repeated_word_in_string.py
@@ -1,3 +1,18 @@
+from collections import Counter 
+ 
+def firstRepeat(input): 
+    words = input.split(' ')
+    dict = Counter(words)
+    for key in words: 
+        if dict[key]>1: 
+            print(key) 
+            return
+    print(key) 
+    return
+
+input = 'Ravi had been saying that he had been there'
+firstRepeat(input) 
+
 class Solution:
 
 #     def firstRepChar(self, s):
